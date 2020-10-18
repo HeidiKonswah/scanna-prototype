27e32525d48886cd3229af22becd1c7a25bc890c
heidikonswah <heidikonswah@gmail.com>
2020-10-18 23:57:19 +0000

    Add services

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Upload from './upload';
 import Header from './header';
+import OurServices from './our-services';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <OurServices />
       <Upload />
     </div>
   );
---
+++ src/our-services.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function OurServices() {
+  return (
+    <div id="services-container">
+        <h1>Our Services</h1>
+        <div id="list">
+        
+        <section>
+            <h3>Title</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris</p>
+        </section>
+        <section>
+            <h3>Title</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris</p>
+        </section>
+        <section>
+            <h3>Title</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris</p>
+        </section>
+        <section>
+            <h3>Title</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris</p>
+        </section>
+        </div>
+        <form>
+            
+        </form>
+    </div>
+  );
+}
+
+export default OurServices;
